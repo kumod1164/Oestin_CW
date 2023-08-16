ad5f85639184809cfdf9774c131fc914a908bce0
kumod1164 <kumodsharma1164@gmail.com>
2023-08-16 15:03:03 +0530

    Contacts completed

---
+++ script/contact_us.js
@@ -0,0 +1,8 @@
+import header from "../components/header.js"
+let head = document.getElementById("header");
+head.innerHTML = header();
+
+import footer1 from "../components/footer.js"
+
+let footer = document.getElementById("footer");
+footer.innerHTML = footer1();
